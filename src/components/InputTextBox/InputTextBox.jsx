de6043d96348f662c57d714820ae7543af841c6c
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-01-30 19:41:14 +0000

    Postcard layout fixed

--- src/components/InputTextBox/InputTextBox.jsx
+++ src/components/InputTextBox/InputTextBox.jsx
@@ -1,13 +1,16 @@
 export default function InputTextBox({ onInputChange, textboxContent }) {
   return (
-    <input
+    <textarea
       type="text"
       id="fname"
       name="fname"
+      rows="4" // Set the height of the textarea
+      cols="35"
+      placeholder="Type your message here..."
       onChange={(event) => {
         onInputChange(event.target.value);
       }}
       value={textboxContent}
-    ></input>
+    ></textarea>
   );
 }
