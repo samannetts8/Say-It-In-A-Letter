04f1dcb6a36f7f9fa45ef6ba617d9984b4a7b644
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-01-30 18:40:27 +0000

    Postcard added, assests folder added

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,17 @@
 import MainMessageBox from "../MainMessageBox/MainMessageBox";
+import "./Board.css";
 
 export default function Board({ MessageBoxContent, fontStyle }) {
   return (
-    <div>
+    <>
       <div>⬅️</div>
-      <MainMessageBox
-        MessageBoxContent={MessageBoxContent}
-        fontStyle={fontStyle}
-      />
+      <div className="postcard-container">
+        <MainMessageBox
+          MessageBoxContent={MessageBoxContent}
+          fontStyle={fontStyle}
+        />
+      </div>
       <div>➡️</div>
-    </div>
+    </>
   );
 }
